19633e0dfed86ca50e594f36784665ba498e6d77
cintaayunda <roswitacinta0@gmail.com>
2025-02-12 11:26:21 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,18 @@
+let namaDepan = "roswita";
+let namaTengah = "cinta";
+let namaBelakang = "ayunda";
+let alamat = "sriwulan sayung";
+let usia = "16 tahun";
+let ttl = "demak,31 maret 2009";
+let gender = "perempuan";
+let agama = "islam";
+let email = "@cayntata"
+let berat = "52";
+let tinggi = "163";
+let hobi = "masak"
+let cita = "bos mudaa";
+let perkerjaan = "pelajar";
+let sekolah = "smk negri 1 sayung";
+let jurusan = "peranangkat lunak/pplg";
+
+document.write ("nama saya adalah " + namaDepan + " " + namaTengah + " " + namaBelakang + " "+" saya tinggal di" + alamat + " pada tahun 2025 usia saya " + usia + " saya lahir di " + ttl + " jenis kelamin saya adalah " + gender + " saya beragama" + agama + " alamat email saya adalah" + email + " saya memiliki berat badan" + berat + " dengan tinggi" + tinggi + " saya sangat senang" + hobi + " karena saya memiliki cita cita " + cita +" saat ini saya adalah seorang" + perkerjaan + " yang bersekolah di" + sekolah +" pada jurusan" + jurusan);
